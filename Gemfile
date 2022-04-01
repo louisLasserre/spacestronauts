0c7604dadae12e6f38f1efa5dc52e2961fc6872a
louislasserre <louislasserre@mmibordeaux.com>
2022-04-01 14:03:52 +0200

    devise base config working

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ gem "sassc-rails"
 
 gem "simple_form"
 
+
+gem 'devise'
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users
+
   resources :travels
   resources :rockets
   resources :companies
@@ -9,7 +12,8 @@ Rails.application.routes.draw do
   get "/travels/:destId/:rocketId", to: "travels#selected"
   get "/updateTravels", to: "travels#updateTravels"
 
-  root "home#index"
+
+  root :to => "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
